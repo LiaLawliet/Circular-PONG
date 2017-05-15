f8401864c8bf5ef51b067e51e6a18f502dc5b5ce
Failyriece <failyriece@gmail.com>
2017-05-15 14:06:16 +0200

    Déplacement des raquettes

--- js/game.js
+++ js/game.js
@@ -1,5 +1,5 @@
-
 var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'phaser-example', { preload: preload, create: create, update: update });
+
 
 function preload() {
 
@@ -18,55 +18,51 @@ function create() {
 
     game.add.image(0, 0, 'sky');
 
-	game.physics.startSystem(Phaser.Physics.P2JS);
-    game.physics.p2.setImpactEvents(true);
-    game.physics.p2.restitution = 0.8;
-
-    var J1CollisionGroup = game.physics.p2.createCollisionGroup();
-    var J2CollisionGroup = game.physics.p2.createCollisionGroup();
-    game.physics.p2.updateBoundsCollisionGroup();
-    game.physics.p2.enable(sprite, false);
-    game.physics.p2.enable(sprite2, false);
-
-
-
-
-
 
     sprite = game.add.sprite(400, 300, 'ball');
     sprite.anchor.setTo(0.5);
-    sprite.pivot.x = 200;
+    sprite.pivot.x =200;
     sprite.angle = 90;
-    sprite.body.setCollisionGroup(J1CollisionGroup);
-    sprite.body.collides([J1CollisionGroup, J2CollisionGroup]);
 
     sprite2 = game.add.sprite(400, 300, 'balu');
     sprite2.anchor.setTo(0.5);
-    sprite2.pivot.x = 200;
+    sprite2.pivot.x =200;
     sprite2.angle = 270;
-    sprite2.body.setCollisionGroup(J2CollisionGroup);
-    sprite2.body.collides([J1CollisionGroup, J2CollisionGroup]);
 
     var vu1 = game.add.sprite(390, 290, 'vu');
-
+    cursorD = game.input.keyboard.addKey(Phaser.Keyboard.D);
+    cursorQ = game.input.keyboard.addKey(Phaser.Keyboard.Q);
     cursors = game.input.keyboard.createCursorKeys();
-
 }
 
 function update() {
-    // if (cursors.left.isDown)
-    // {
-    //     sprite.angle -= 2;
-    // }
-    // else if (cursors.right.isDown)
-    // {
-    //     sprite.angle += 2;
-    // }
-    // if (cursors.up.isDown) {
-    //     sprite2.angle -= 2;
-    // }else if (cursors.down.isDown) {
-    //     sprite2.angle += 2;
-    // }
+    var s1am2 = sprite.angle-2+180;
+    var s1ap2 = sprite.angle+2+180;
+    var s1amX = sprite.angle-13+180;
+    var s1apX = sprite.angle+13+180;
+    var s2am2 = sprite2.angle-2+180;
+    var s2ap2 = sprite2.angle+2+180;
+    var s2amX = sprite2.angle-13+180;
+    var s2apX = sprite2.angle+13+180;
+
+    if (cursors.right.isDown)
+    {
+        if (s1ap2 > s2amX && s1ap2 < s2apX) {
+        }else{sprite.angle += 2;}
+    }
+    else if (cursors.left.isDown)
+    { 
+        if (s1am2 > s2amX && s1am2 < s2apX) {
+        }else{sprite.angle -= 2;}
+    }
+    if (cursorQ.isDown) {
+        if (s2ap2 > s1amX && s2ap2 < s1apX) {
+        }else{sprite2.angle += 2;}
+    }
+    else if (cursorD.isDown) {
+        if (s2am2 > s1amX && s2am2 < s1apX) {
+        }else{sprite2.angle -= 2;}
+    }
 
 
 }
