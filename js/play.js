049cf7d9cde833f2d326ef24783ecbe808632161
Failyriece <failyriece@gmail.com>
2017-05-22 12:11:11 +0200

    MVP 1.0

--- js/play.js
+++ js/play.js
@@ -17,6 +17,9 @@ var playState = function (){
     s2180 = null;
     cercle = null;
     balle = null;
+    ballepos = null;
+    balleang = null;
+    ballecouleur = 0x555555;
     coballe = {
         x : 400,
         y : 300,
@@ -37,7 +40,7 @@ playState.prototype ={
         graphics.drawCircle(400, 300, 405);
 
         balle = game.add.graphics(0, 0);
-        balle.beginFill(0x555555, 1);
+        balle.beginFill(ballecouleur, 1);
         balle.drawCircle(coballe.x, coballe.y, 15);
 
         sprite = game.add.sprite(400, 300, 'ball');
@@ -57,6 +60,7 @@ playState.prototype ={
     },
 
     update:function () {
+        // Changement des variables
         s1am2 = sprite.angle-vitesseraquette+180;
         s1ap2 = sprite.angle+vitesseraquette+180;
         s1amX = sprite.angle-13+180;
@@ -67,20 +71,39 @@ playState.prototype ={
         s2apX = sprite2.angle+13+180;
         s1180 = sprite.angle+180;
         s2180 = sprite2.angle+180;
-        coballe.x += coballe.xdir;
-        coballe.y += coballe.ydir;
+        ballepos = Math.sqrt(Math.pow((coballe.x-400),2)+Math.pow((coballe.y - 300),2));
+        balleang = (Math.atan2(coballe.x-400,coballe.y-300)*180)/Math.PI*-1+90;
+        if (balleang<0) {balleang+=360;}
+ 
 
-         if (checkOverlap(balle, sprite))
-           {
-                console.log('Bulu');
-           }
-
-        if (Math.sqrt(Math.pow((coballe.x-400),2)+Math.pow((coballe.y - 300),2)) < 250) {
-        balle.clear();
-        balle.beginFill(0x555555, 1);
-        balle.drawCircle(coballe.x, coballe.y, 15);
+        // Touche de raquette
+        if ((ballepos<202&&ballepos>198)&&(balleang > s1amX && balleang < s1apX)) {
+            coballe.xdir = coballe.xdir*-1;
+            coballe.ydir = coballe.ydir*-1;
+            ballecouleur = 0x500000;
         }
 
+        if ((ballepos<202&&ballepos>198)&&(balleang > s2amX && balleang < s2apX)) {
+            coballe.xdir = coballe.xdir*-1;
+            coballe.ydir = coballe.ydir*-1;
+            ballecouleur = 0x000050;
+        }
+
+
+        // Mouvement de la balle
+        if (ballepos < 250) {
+            coballe.x += coballe.xdir;
+            coballe.y += coballe.ydir;
+            balle.clear();
+            balle.beginFill(ballecouleur, 1);
+            balle.drawCircle(coballe.x, coballe.y, 15);
+        }
+
+
+
+
+        // Mouvement des raquettes
+            // J1
         if (cursors.right.isDown)
         {
             if (s1ap2>s2amX&&s1ap2<s2apX||(s1180+vitesseraquette)-s2180>345) {
@@ -93,6 +116,7 @@ playState.prototype ={
             if ((s1am2 > s2amX && s1am2 < s2apX)||(360-s2180+s1180<16 && s2180 > 345)) {
             }else{sprite.angle -= vitesseraquette;}
         }
+            // J2
         if (cursorD.isDown) {
             if ((s2ap2 > s1amX && s2ap2 < s1apX)||(s2180+vitesseraquette)-s1180>345) {
             }else{sprite2.angle += vitesseraquette;}
@@ -104,11 +128,4 @@ playState.prototype ={
     }
     
 
-};function checkOverlap(spriteA, spriteB) {
-    
-        var boundsA = spriteA.getBounds();
-        var boundsB = spriteB.getBounds();
-    
-        return Phaser.Rectangle.intersects(boundsA, boundsB);
-    
-    }
+};
